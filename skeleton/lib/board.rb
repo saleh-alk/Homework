275acca5d773b2bbe7f876131a5a107b235f061d
Saleh Alkaheli <skalkaheli@gmail.com>
2022-10-24 22:14:52 -0700

    Mancala Project

---
+++ skeleton/lib/board.rb
@@ -0,0 +1,62 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    four_stone_cup =  [:stone, :stone, :stone, :stone]
+    @cups = Array.new(14){Array.new(four_stone_cup)}
+    @cups[6] = []
+    @cups[13] = []
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+  end
+
+  def valid_move?(start_pos)
+    if start_pos > 13 || start_pos < 0
+      raise 'Invalid starting cup'
+    elsif @cups[start_pos].length == 0
+      raise 'Starting cup is empty'
+    end
+  end
+
+  def make_move(start_pos, current_player_name)
+    k = @cups[start_pos].length
+    @cups[start_pos] = []
+    (start_pos+1..start_pos+k).each do |i|
+      if i != 13
+        @cups[i] << :stone
+        if @cups[i].length == 1
+          return next_turn(i)
+        end
+      end
+    end
+    render
+
+  end
+
+  def next_turn(ending_cup_idx)
+    return ending_cup_idx
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    return true if @cups[0..5].all?{|ele| ele.length == 0} || @cups[7..12].all?{|ele| ele.length == 0}
+    false
+  end
+
+  def winner
+    return :draw if @cups[6].length == 6 && @cups[13].length == 6
+  end
+end
+
+
+
