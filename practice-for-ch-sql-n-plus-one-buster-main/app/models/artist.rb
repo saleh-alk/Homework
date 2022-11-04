0650e75818fb05a86226d618f14d98e55eaf9e84
Saleh Alkaheli <skalkaheli@gmail.com>
2022-11-03 22:56:23 -0700

    N+1 HW

---
+++ practice-for-ch-sql-n-plus-one-buster-main/app/models/artist.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id,
+    dependent: :destroy
+
+  def n_plus_one_tracks
+    my_albums = self.albums
+    track_counts = {}
+    my_albums.each do |album|
+      track_counts[album.title] = album.tracks.length
+    end
+
+    track_counts
+  end
+
+  def better_tracks_query
+    albums = Artist.albums.select("albums.*, COUNT(*) AS tracks_count").joins(:tracks).groups(album_id)
+    counts = {}
+    albums.each do |album|
+      counts[album.title] = album.tracks_count
+    end
+
+    counts
+  end
+
+end
